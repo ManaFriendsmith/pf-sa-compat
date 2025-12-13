7b9a5e08370b93f022ef19f62b87e768a1868ab9
ManaFriendsmith <42ah42@gmail.com>
2025-12-12 19:08:39 -0500

    recycler resize reimplement

--- data-updates.lua
+++ data-updates.lua
@@ -427,4 +427,17 @@ require("compat.small-mod")
 
 if mods["quality"] and misc.last_pf_mod == "pf-sa-compat" then
     require("__quality__/data-updates.lua")
+
+    local biggest_result_list = data.raw.furnace.recycler.result_inventory_size
+    for k, v in pairs(data.raw.recipe) do
+      if v.type == "recycling" or v.type == "recycling-or-hand-crafting" then
+        if v.results and #v.results > biggest_result_list then
+          biggest_result_list = #v.results
+        end
+      end
+    end
+    data.raw.furnace.recycler.result_inventory_size = biggest_result_list
+    if mods["Age-of-Production"] then
+        data.raw.furnace["aop-salvager"].result_inventory_size = biggest_result_list
+    end
 end
